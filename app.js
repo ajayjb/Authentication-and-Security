e3ec3258340a74dfb70b73d090d46af9a7cb876f
Ajay <ajayjb11@gmail.com>
2021-09-13 18:41:45 +0530

    .env file added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -14,13 +15,13 @@ app.set("view engine", "ejs");
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
+
 const userSchema = new mongoose.Schema({
   email : String,
   password : String
 });
 
-const secret = "This is our little secrets"
-userSchema.plugin(encrypt, {secret : secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:["password"]});
 
 const User = mongoose.model("User", userSchema);
 
